0298026d4f54386d4df62d25c8ee5f1b0ffc4fc6
Aniket2812 <aniketyadav982@gmail.com>
2026-08-31 22:05:20 +0530

    fix(aws): trust GitHub production environment OIDC

--- deploy/aws/terraform/iam.tf
+++ deploy/aws/terraform/iam.tf
@@ -76,7 +76,7 @@ resource "aws_iam_role" "github_deploy" {
       Condition = {
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/${var.github_branch}"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:environment:production"
         }
       }
     }]
